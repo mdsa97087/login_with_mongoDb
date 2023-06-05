afcffbaa117e287cef7dc184eaf58fa9531c79a6
mdsa97087 <mdsa97087@gmail.com>
2023-06-05 21:28:10 +0530

    login

--- src/app.js
+++ src/app.js
@@ -10,12 +10,21 @@ const static_path=path.join(__dirname,"../public")
 console.log(path.join(__dirname,"../public"))
 app.use(express.static(static_path))
 
+//  Add index. hbs page
+
+app.set("view engine", "hbs")
+
 *// 
 
 
 
 app.get("/", (req,res) => {
-      res.send("hello i am leaning")
+      // res.send("hello i am leaning")
+
+//  for index.hbs page
+
+      res.render("index")
+
 })
 
 
--- src/db/conn.js
+++ src/db/conn.js
@@ -4,9 +4,9 @@ const mongoose=require("mongoose")
 mongoose.connect("mongodb://localhost:27017/youtubeRegistration", {
       useNewUrlParser:true,
       useUnifiedTopology:true,
-      useCreateIntex:true
+      useCreateIndex:true
 }).then(() => {
       console.log(`connectio ok`)
 }).catch((e) => {
-      console.log("not ok h")
+      console.log("not ok ")
 })
